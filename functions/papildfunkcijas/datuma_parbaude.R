c3f1ee6c6bc4cd4719de64ec0fb2d16a7f62c408
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-25 22:28:31 +0300

    Update datuma_parbaude.R

--- functions/papildfunkcijas/datuma_parbaude.R
+++ functions/papildfunkcijas/datuma_parbaude.R
@@ -1,1 +1,17 @@
+datuma_parbaude <- function(y) {
+ceturksna_datums = switch(
+  as.character(Q),
+  "1" = paste0("03", substr(year, 3, 4)),
+  "2" = paste0("06", substr(year, 3, 4)), 
+  "3" = paste0("09", substr(year, 3, 4)),
+  "4" = paste0("12", substr(year, 3, 4)) 
+)
 
+if(sum(y$DAT == ceturksna_datums) == nrow(y)) {
+  atb <- paste0("PĀRBAUDE IZIETA: Datnē norādītais datums ", ceturksna_datums, " atbilst.")
+} else {
+  stop("Datnē datumi neatbilst.")
+}
+
+return(atb)
+}
